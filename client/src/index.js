3f30788a880fa2df7a50cccb1ca835f6091fa3f3
lozzoc <mr.madbozo@gmail.com>
2020-02-01 17:37:07 -0800

    inital commit

---
+++ client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+
+
+
+
+class Homepage extends React.Component {
+
+
+  render(){
+
+    return (<div></div>)
+  }
+}
+
+
+
+
+
+
+
+
+
+
+ReactDOM.render(<Homepage />, document.getElementById('root'));
